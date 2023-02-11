605cc9d2be8b77b1ccf61cdee7025c995fe97f77
Nouran kadri <101168150+engnouran12@users.noreply.github.com>
2023-02-11 20:14:04 +0200

    trig addnote cubuit

--- lib/cubit/add-note_cubit/add_note_cubit.dart
+++ lib/cubit/add-note_cubit/add_note_cubit.dart
@@ -1,24 +1,23 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constants.dart';
 
+import '../../models/note_model.dart';
 import 'add_note_state.dart';
-class AddNoteCubit extends Cubit<AddNoteState>{
-   AddNoteCubit() : super(AddNoteInitial());
-   
 
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteInitial());
+  addnote(NoteModel note) async {
+    emit(AddNoteLoading());
 
-
-
-
-
-
-
-
-
-
-
-
-
-  
+    try {
+      var notesbox = Hive.box<NoteModel>(knotes);
+      emit(AddNoteSuccess());
+      await notesbox.add(note);
+    } on Exception catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+  }
 }
